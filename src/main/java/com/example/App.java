df6c7c1a9a5b56684cc94570e2fcf2a00b715d96
Hugo Raskin <raskin.hugo@gmail.com>
2025-07-09 11:46:42 +0200

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,9 @@
 
 public class App {
     public static void main(String[] args) {
-
+        System.out.println("Hello world!");
+        int var1 = 0;
+        int var2 = 5;
+        int var3 = var1 / var2;
     }
 }
