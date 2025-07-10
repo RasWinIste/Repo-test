26c7f2ac3ae1d3f15346f71652b01a714833e8b5
Hugo Raskin <raskin.hugo@gmail.com>
2025-07-10 17:03:20 +0200

    Update Module1.java

--- module1/src/main/java/com/acme/module1/Module1.java
+++ module1/src/main/java/com/acme/module1/Module1.java
@@ -4,6 +4,12 @@ public class Module1 {
 
   public void coveredByUnitTest() {
     System.out.println("This method is covered by unit test");
+    System.out.println("Just adding random stuff to get value");
+
+    int var_1 = 0;
+    int var_2 = 2;
+
+    int var_3 = var_2 / var_1;
   }
 
   public void coveredByIntegrationTest() {
