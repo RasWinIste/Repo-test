a1dea191f4ff869e24d5ccf9d8049e213cc9f165
Hugo Raskin <raskin.hugo@gmail.com>
2025-07-11 10:27:46 +0200

    New Pull Request

--- module1/src/test/java/com/acme/module1/Module1Test.java
+++ module1/src/test/java/com/acme/module1/Module1Test.java
@@ -7,6 +7,13 @@ public class Module1Test {
   @Test
   public void coveredByUnitTest() {
     Module1 module1 = new Module1();
+
+    System.out.println("Juste un test");
+    int var0 = 0;
+    int var1 = 3;
+
+    int var2 = var1 / var0;
+    
     module1.coveredByUnitTest();
   }
 }
