e76a91ce0bb24647fd8730d3f2eb4166fe3e90ff
Hugo Raskin <raskin.hugo@gmail.com>
2025-07-11 09:10:23 +0200

    test

--- module1/src/main/java/com/acme/module1/Module1.java
+++ module1/src/main/java/com/acme/module1/Module1.java
@@ -4,6 +4,7 @@ public class Module1 {
 
   public void coveredByUnitTest() {
     System.out.println("This method is covered by unit test");
+    System.out.println("Just testing");
   }
 
   public void coveredByIntegrationTest() {
