e9d7fd002b290cea8ecf60b09f2a973f76361a95
Hugo Raskin <raskin.hugo@gmail.com>
2025-07-14 11:04:48 +0200

    Update Module1.java

--- module1/src/main/java/com/acme/module1/Module1.java
+++ module1/src/main/java/com/acme/module1/Module1.java
@@ -5,6 +5,10 @@ public class Module1 {
   public void coveredByUnitTest() {
     System.out.println("This method is covered by unit test");
     System.out.println("Just testing");
+
+    System.out.println("Just testing");
+
+    
     System.out.println("CodeQL");
   }
 
