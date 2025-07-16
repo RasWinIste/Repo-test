615a3606e2052474d1f469c69f6fcc408dc2eabb
Hugo Raskin <raskin.hugo@gmail.com>
2025-07-16 10:41:24 +0200

    Big PR

--- module1/src/main/java/com/acme/module1/Module1.java
+++ module1/src/main/java/com/acme/module1/Module1.java
@@ -4,8 +4,35 @@ public class Module1 {
 
   public void coveredByUnitTest() {
     System.out.println("This method is covered by unit test");
-    System.out.println("Just testing");
-    System.out.println("CodeQL");
+    
+    System.out.println("By pr for testing SonarCloud new line coverage");
+
+    System.out.println("Calculating Fibonacci just for fun");
+    int n = 10, firstTerm = 0, secondTerm = 1;
+    System.out.println("Fibonacci Series till " + n + " terms:");
+
+    for (int i = 1; i <= n; ++i) {
+      System.out.print(firstTerm + ", ");
+
+      // compute the next term
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
+    }
+
+    int var_0 = 1;
+    int var_1 = 2;
+    int var_3 = var_0 - var_1;
+
+    System.out.print("Calculating for fun : " + var_3);
+    System.out.print("Hope you like this test");
+
+    int var_0 = 1;
+    int var_1 = 2;
+    int var_3 = var_0 - var_1;
+
+    System.out.print("Calculating for fun : " + var_3);
+    System.out.print("Hope you like this test");
   }
 
   public void coveredByIntegrationTest() {
